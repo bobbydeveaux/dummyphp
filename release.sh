40a754783584663e8d2b2b697a0e54058a382ab5
Bobby DeVeaux <bdeveaux@ukcloud.com>
2016-12-14 12:38:37 +0000

    pushing release changes

--- release.sh
+++ release.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-export VERSION='1.0.0'
+export VERSION='1.0.6'
 
 git add . 
 git commit -m 'pushing release changes'
 git tag -a $VERSION -m 'tagging release'
 docker-compose build 
 
-docker commit 1c582911c34d bobbydvo/lemp_php-fpm:$VERSION
-docker commit 1c582911c34d bobbydvo/lemp_php-fpm:latest
+docker tag bobbydvo/lemp_php-fpm:latest  bobbydvo/lemp_php-fpm:$VERSION
+docker tag bobbydvo/lemp_nginx:latest    bobbydvo/lemp_nginx:$VERSION
 
 docker push bobbydvo/lemp_php-fpm:$VERSION
-docker push bobbydvo/lemp_php-fpm:latest
+docker push bobbydvo/lemp_nginx:$VERSION
 
- #ssh core@51.179.219.86 "date && hostname"
+ ssh core@51.179.219.30 "date && hostname"

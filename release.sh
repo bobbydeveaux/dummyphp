3a593612cf808c9605fd52c59d5bbe2e7066b6db
Bobby DeVeaux <bdeveaux@ukcloud.com>
2016-12-15 12:31:49 +0000

    pushing release changes

--- release.sh
+++ release.sh
@@ -5,7 +5,9 @@ export VERSION='1.0.14'
 git add . 
 git commit -m 'pushing release changes'
 
+# This stuff to happen on CI server.
 set -e
+# git clone the repo.
 git tag -a $VERSION -m 'tagging release'
 git push 
 git push --tags
@@ -13,7 +15,6 @@ docker-compose build
 
 docker tag bobbydvo/lemp_php-fpm:latest  bobbydvo/lemp_php-fpm:$VERSION
 docker tag bobbydvo/lemp_nginx:latest    bobbydvo/lemp_nginx:$VERSION
-
 docker push bobbydvo/lemp_php-fpm:$VERSION
 docker push bobbydvo/lemp_nginx:$VERSION
 docker push bobbydvo/lemp_php-fpm:latest
